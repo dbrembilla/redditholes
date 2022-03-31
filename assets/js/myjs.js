70c8311bbf8155d0fa98af194a160b690b8de7d2
giorgiasampo <giorgia.sampo@studio.unibo.it>
2022-03-31 11:12:38 +0200

    b

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,7 +1,7 @@
 document.addEventListener(
     'scroll',
     (event) => {
-        document.getElementsByClassName("nav-link scrollto active")[0].classList.add("visible")
+        document.getElementsByClassName("nav-link scrollto active")[0].classList.add("visible");
     }, 
     { passive: true }
 );
