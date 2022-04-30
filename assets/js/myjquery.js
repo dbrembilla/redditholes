05100b92460b484544c41cf7c73c3aeff61de11d
giorgiasampo <giorgia.sampo@studio.unibo.it>
2022-04-30 08:31:13 +0200

    png in place, file jquery, modified index

---
+++ assets/js/myjquery.js
@@ -0,0 +1,5 @@
+$(window).on("load",function(){
+    $(".loader-wrapper-wrapper").fadeOut("slow");
+    $('body').css({'overflow':'auto', 'height':'auto', 'position':'relative'});
+    $('.logo').css({'visibility':'visible'});
+  });
